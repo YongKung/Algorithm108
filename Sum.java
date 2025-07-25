0f5eeaa1bb3ef274f298c9615433029af7025675
YongKung <giantgamer1321@gmail.com>
2025-07-25 14:40:25 +0700

    First commit

---
+++ Sum.java
@@ -0,0 +1,16 @@
+public class Sum{
+    public static void main(String args[]){
+
+        int num1 = 4;
+
+        int num2 = 6;
+
+        int sum=num1+num2;
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+
+
+
+    }
+}
