760ed830bec8aad93da2e50b27275824a538871e
YongKung <giantgamer1321@gmail.com>
2025-07-25 16:23:05 +0700

    วงกลม

--- Circle.java
+++ Circle.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Circle {
     public static void main(String[] args) {
-        
     Scanner input = new Scanner(System.in);
     
 
-    System.out.print("ป้อนข้อความ: ");
+    System.out.print("ใส่รัศมีวงกลม (เมตร): ");
+    int Raduis = input.nextInt();    
+        
+    int Area=( 22 / 7 ) * Raduis * Raduis ;
 
-
-
+    System.out.print("พื้นที่วงกลม = " + Area + " ตารางเมตร");
 
     }
 }
